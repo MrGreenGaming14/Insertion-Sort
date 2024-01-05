c06ad02a4eb14ea34d90237a57139672d7bc1069
MrGreenGaming14 <ronaldkgibson3@gmail.com>
2024-01-04 20:02:22 -0600

    The Insertion Sort regular works. I will just add a couple of features

--- Insertion-Sort.c
+++ Insertion-Sort.c
@@ -1,8 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+/**
+ * Is the array sorted
+ */
+int isSorted(int array[]) {
   for (int i = 0; i < 10; i++) {
-    printf("Hello World");
+    if (array[i] > array[i + 1]) {
+      return 0;
+    }
   }
+
+  return 1;
+}
+
+/**
+ * Prints the full array
+ */
+void printArray(int array[], int size, char *expression) {
+  printf("\n%s: [", expression);
+  for (int i = 0; i < size; i++) {
+    printf("%d", array[i]);
+
+    // Print a comma after each element
+    if (i != size - 1) {
+      printf(", ");
+    }
+  }
+  printf("]");
+}
+
+/**
+ *  Partition the array
+ */
+void Partition(int arrayFull[], int arrayShort[]) {}
+
+/**
+ * Insertion Sort but *recursive*
+ */
+void insertionSortRecursive() {}
+
+/*
+ * Sort the array
+ */
+void insertionSort(int array[], int size) {
+  // Size of partition to sort increases each iteration
+  for (int i = 1; i < size; i++) {
+    // Sort the iteration
+    printArray(array, size, "The partitioned array is");
+    int count = i;
+    while (array[count] < array[count - 1] && count > 0) {
+      int tmp = array[count];
+      array[count] = array[count - 1];
+      array[count - 1] = tmp;
+      if(count > 0) {
+        count = count - 1;
+      }
+    }
+
+    printArray(array, size, "The sorted partitioned array is");
+  }
+}
+
+/**
+ * Make an array of size x
+ */
+void makeArray(int array[], int size) {
+  // What are the elements in the array
+  for (int i = 0; i < size; i++) {
+    // Put random numbers in each element
+    int random = rand() % 50;
+    array[i] = random;
+  }
+}
+
+int main() {
+  // How big is the array
+  printf("How big is your array? ");
+  int size;
+  scanf("%d", &size);
+  int array[size];
+  printf("\n");
+
+  // Call the functions
+  makeArray(array, size);
+  printArray(array, size, "The initial array is");
+  insertionSort(array, size);
+  printArray(array, size, "The final array is");
 }
